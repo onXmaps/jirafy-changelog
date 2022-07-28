14d33a24fdedc053f6a2fd06a14807679d96e2b3
Colt Dorsey <colt.dorsey@onxmaps.com>
2022-07-28 13:57:35 -0500

    cypress 10 migration

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'kuhc36',
+  videoUploadOnPasses: false,
+  env: {
+    TEST_JIRA_HOST: 'arglebargle.atlassian.net',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
